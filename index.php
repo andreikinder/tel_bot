fbf53af5523bd7035f6c38b0a99445dae836e765
andre_gthv1mg <anrei.kinder.170@gmail.com>
2020-11-05 18:28:29 +0500

    init telegram v 2.0

--- index.php
+++ index.php
@@ -4,20 +4,15 @@ require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '1306244872:AAEcthEZKPgA1tT96DekoZVHQU2td-iMltk';
 $bot_username = 'news_caf_bus_bot';
-$hook_url     = 'https://newscbi.herokuapp.com/hook.php';
 
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
-    // Set webhook
-    $result = $telegram->setWebhook($hook_url);
-    if ($result->isOk()) {
-        $result->getDescription();
-        print_r($result);
-    }
+    // Handle telegram webhook request
+    $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // Silence is golden!
     // log telegram errors
-     echo $e->getMessage();
-    echo 'eror';
+    // echo $e->getMessage();
 }
