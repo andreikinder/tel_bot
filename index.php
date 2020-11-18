af58a011b98d479e3f69b867c82e7e8afbec4bd1
andre_gthv1mg <anrei.kinder.170@gmail.com>
2020-11-19 00:09:48 +0500

    telegram v 2.6

--- index.php
+++ index.php
@@ -33,7 +33,8 @@ try {
             //header('Content-type: text/html; charset=utf-8');
             $wall_id="-69632488"; // Положительное число: пользователь. Отрицательное: группа.
             $count="2"; // Количество записей, которое необходимо получить. Максимальное значение: 100.
-            $api = file_get_contents("http://api.vk.com/method/wall.get?owner_id={$wall_id}&count={$count}");
+            $ACCESS_TOKEN ='1397aac61397aac61397aac6c213e2ab25113971397aac64c22dbfee61060bf0a30780f';
+            $api = file_get_contents("http://api.vk.com/method/wall.get?owner_id={$wall_id}&count={$count}&access_token={$ACCESS_TOKEN}&v=5.62");
             $wall = json_decode($api,true);
             foreach ($wall['response'] as $item => $result) {
                 $send_text = '<p>' . $result['text'].'</p>';
